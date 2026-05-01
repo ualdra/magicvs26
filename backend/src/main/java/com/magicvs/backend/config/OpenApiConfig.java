a327331a6fa6c053d21721059d6e10fda81fcaff
Alejandro Avilés Fernández <aaf925@inlumine.ual.es>
2026-05-01 23:23:01 +0200

    Implementación de documentación interactiva de la API con Swagger

---
+++ backend/src/main/java/com/magicvs/backend/config/OpenApiConfig.java
@@ -0,0 +1,24 @@
+package com.magicvs.backend.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(
+    info = @Info(
+        title = "MagicVS API",
+        description = "API REST para la plataforma de combates MagicVS",
+        version = "1.0"
+    )
+)
+@SecurityScheme(
+    name = "bearerAuth",
+    type = SecuritySchemeType.HTTP,
+    bearerFormat = "JWT",
+    scheme = "bearer"
+)
+public class OpenApiConfig {
+}
